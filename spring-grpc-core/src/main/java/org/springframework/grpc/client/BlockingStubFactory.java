fe9fcbfa480ff329c2113ec819bf9e83d6fc9974
gaattc <gaattc000@gmail.com>
2025-06-25 15:37:29 +0800

    #203 compatible for old grpc version
    Signed-off-by: gaattc <gaattc000@gmail.com>

--- spring-grpc-core/src/main/java/org/springframework/grpc/client/BlockingStubFactory.java
+++ spring-grpc-core/src/main/java/org/springframework/grpc/client/BlockingStubFactory.java
@@ -19,14 +19,15 @@ import io.grpc.stub.AbstractBlockingStub;
 
 public class BlockingStubFactory extends AbstractStubFactory<AbstractBlockingStub<?>> {
 
-	public static boolean supports(Class<?> type) {
-		return AbstractStubFactory.supports(AbstractBlockingStub.class, type)
-				&& !type.getSimpleName().contains("BlockingV2");
-	}
+    public static boolean supports(Class<?> type) {
+        return (AbstractStubFactory.supports(AbstractBlockingStub.class, type) && !type.getSimpleName().contains("BlockingV2"))
+                // compatible for old grpc version
+                || type.getSimpleName().endsWith("BlockingStub");
+    }
 
-	@Override
-	protected String methodName() {
-		return "newBlockingStub";
-	}
+    @Override
+    protected String methodName() {
+        return "newBlockingStub";
+    }
 
 }
