d2c7211453f9c5ee111ac88d9c83b711b2a8b7b6
onobc <chris.bono@gmail.com>
2025-12-11 22:58:29 -0600

    Bind to configured host on Netty servers
    This adjusts the Netty (shaded and non-shaded) server factories to
    bind to the specific hostname when configured rather than
    always bind to all IP addresses. When the configured hostname is the
    wildcard then it binds to all IP addresses.
    
    Closes #319
    
    Co-authored-by: Ivan Lorca <ivan@ivan.cat>
    Signed-off-by: onobc <chris.bono@gmail.com>

--- spring-grpc-core/src/test/java/org/springframework/grpc/internal/GrpcUtilsTests.java
+++ spring-grpc-core/src/test/java/org/springframework/grpc/internal/GrpcUtilsTests.java
@@ -17,43 +17,83 @@
 package org.springframework.grpc.internal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class GrpcUtilsTests {
 
-	@Test
-	void testGetPortFromAddress() {
-		assertThat(GrpcUtils.getPort("localhost:8080")).isEqualTo(8080);
+	@DisplayName("GetPortShouldReturn")
+	@ParameterizedTest(name = "{1} for address {0}") // GH-319
+	@MethodSource
+	void returnProperPort(String address, int expectedPort) {
+		assertThat(GrpcUtils.getPort(address)).isEqualTo(expectedPort);
 	}
 
-	@Test
-	void testGetNoPort() {
-		assertThat(GrpcUtils.getPort("localhost")).isEqualTo(9090);
+	static Stream<Arguments> returnProperPort() {
+		return Stream.of(arguments("*", GrpcUtils.DEFAULT_PORT), arguments("*:9090", 9090),
+				arguments("localhost", GrpcUtils.DEFAULT_PORT), arguments("localhost:9999", 9999),
+				arguments("example.com:1234/path", 1234), arguments("example.com/path", GrpcUtils.DEFAULT_PORT),
+				arguments("127.0.0.1", GrpcUtils.DEFAULT_PORT), arguments("127.0.0.1:8080", 8080),
+				arguments("foo.googleapis.com", GrpcUtils.DEFAULT_PORT), arguments("foo.googleapis.com:8080", 8080),
+				arguments("[::1]", GrpcUtils.DEFAULT_PORT), arguments("[::1]:8080", 8080),
+				arguments("localhost:xxx", GrpcUtils.DEFAULT_PORT),
+				arguments("[2001:db8:85a3:8d3:1319:8a2e:370:7348]", GrpcUtils.DEFAULT_PORT),
+				arguments("[2001:db8:85a3:8d3:1319:8a2e:370:7348]:443", 443));
 	}
 
-	@Test
-	void testGetPortFromAddressWithPath() {
-		String address = "example.com:1234/path";
-		assertThat(GrpcUtils.getPort(address)).isEqualTo(1234);
+	@DisplayName("GetPortShouldReject")
+	@ParameterizedTest(name = "address {0} because {1}") // GH-319
+	@MethodSource
+	void rejectGetPortForInvalidAddress(String address, String startOfExceptionMessage) {
+		assertThatIllegalArgumentException().isThrownBy(() -> GrpcUtils.getPort(address))
+			.withMessageStartingWith(startOfExceptionMessage);
 	}
 
-	@Test
-	void testGetDomainAddress() {
-		String address = "unix:/some/file/somewhere";
-		assertThat(GrpcUtils.getPort(address)).isEqualTo(-1);
+	static Stream<Arguments> rejectGetPortForInvalidAddress() {
+		return Stream.of(arguments("unix:/some/file/somewhere", "Unix domain socket addresses not supported"),
+				arguments("tcp://foo:9090", "Addresses with schemas are not supported"),
+				arguments("static://foo:9090", "Addresses with schemas are not supported"),
+				arguments("http://localhost", "Addresses with schemas are not supported"));
 	}
 
-	@Test
-	void testGetStaticSchema() {
-		String address = "static://localhost";
-		assertThat(GrpcUtils.getPort(address)).isEqualTo(9090);
+	@DisplayName("GetHostnameShouldReturn")
+	@ParameterizedTest(name = "{1} for address {0}") // GH-319
+	@MethodSource
+	void returnProperHostname(String address, String expectedHostname) {
+		assertThat(GrpcUtils.getHostName(address)).isEqualTo(expectedHostname);
 	}
 
-	@Test
-	void testGetInvalidAddress() {
-		String address = "invalid:broken";
-		assertThat(GrpcUtils.getPort(address)).isEqualTo(9090); // -1?
+	static Stream<Arguments> returnProperHostname() {
+		return Stream.of(arguments("*", "*"), arguments("*:9090", "*"), arguments("localhost", "localhost"),
+				arguments("localhost:9999", "localhost"), arguments("example.com:1234/path", "example.com"),
+				arguments("example.com/path", "example.com"), arguments("127.0.0.1", "127.0.0.1"),
+				arguments("127.0.0.1:8080", "127.0.0.1"), arguments("foo.googleapis.com", "foo.googleapis.com"),
+				arguments("foo.googleapis.com:8080", "foo.googleapis.com"), arguments("[::1]", "[::1]"),
+				arguments("[::1]:8080", "[::1]"),
+				arguments("[2001:db8:85a3:8d3:1319:8a2e:370:7348]", "[2001:db8:85a3:8d3:1319:8a2e:370:7348]"),
+				arguments("[2001:db8:85a3:8d3:1319:8a2e:370:7348]:443", "[2001:db8:85a3:8d3:1319:8a2e:370:7348]"));
+	}
+
+	@DisplayName("GetHostnameShouldReject")
+	@ParameterizedTest(name = "address {0} because {1}") // GH-319
+	@MethodSource
+	void rejectGetHostnameForInvalidAddress(String address, String startOfExceptionMessage) {
+		assertThatIllegalArgumentException().isThrownBy(() -> GrpcUtils.getHostName(address))
+			.withMessageStartingWith(startOfExceptionMessage);
+	}
+
+	static Stream<Arguments> rejectGetHostnameForInvalidAddress() {
+		return Stream.of(arguments("unix:/some/file/somewhere", "Unix domain socket addresses not supported"),
+				arguments("tcp://foo:9090", "Addresses with schemas are not supported"),
+				arguments("static://foo:9090", "Addresses with schemas are not supported"),
+				arguments("http://localhost", "Addresses with schemas are not supported"));
 	}
 
 }
