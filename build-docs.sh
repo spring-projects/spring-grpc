70522413dd98b24ff8a299b023cff0164290bdde
Dave Syer <david.syer@broadcom.com>
2026-04-24 08:38:14 +0100

    Fix docs maybe

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Generate the README.md, CONTRIBUTING.md and the Antora site
-./mvnw -pl spring-grpc-docs package antora docs
+./mvnw package -DskipTests
+./mvnw -pl spring-grpc-docs package antora
